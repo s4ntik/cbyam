44cf04e654a56b6223832586e887e433b1de6b77
s4ntik <145198614+s4ntik@users.noreply.github.com>
2023-11-19 23:42:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     const targetTimeToday = moment().set({
-        hour: 9,
+        hour: 8,
         minute: 0,
         second: 0,
         millisecond: 0,
