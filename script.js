f0846c5acc8edefc907b3be82a1e45f302dcbf22
s4ntik <145198614+s4ntik@users.noreply.github.com>
2024-03-23 23:11:26 +0000

    Revert to 1.0

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         const siteStatus = document.getElementById("json-data");
-        const lovense = document.getElementById("app");
         const brb = document.querySelector(".centered-text");
         const bigTextElement = document.querySelector(".big-text");
         const progressBar = document.querySelector(".progress-bar");
@@ -35,9 +34,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
             superbowl.style.visibility = data.gamemode === "true" ? "visible" : "hidden";
             superbowl.style.opacity = data.gamemode === "true" ? "1" : "0";
-
-            lovense.style.visibility = data.app === "true" ? "visible" : "hidden";
-            lovense.style.opacity = data.app === "true" ? "1" : "0";
 
             timer.style.visibility = data.timer === "true" ? "visible" : "hidden";
             timer.style.opacity = data.timer === "true" ? "1" : "0";
